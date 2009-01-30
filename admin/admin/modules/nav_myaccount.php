d07dbc129bf369ea6bac0280760f50691223ae0e
scottsweb <scottsweb@d6a5f73a-7f42-0410-bfaf-71582268690a>
2009-01-30 18:58:37 +0000

    Pixie 1.01

---
+++ admin/admin/modules/nav_myaccount.php
@@ -0,0 +1,19 @@
+<?php
+//*****************************************************************//
+// Pixie: The Small, Simple, Site Maker.                           //
+// ----------------------------------------------------------------//
+// Licence: GNU General Public License v3                   	   //
+// Title: Nav_Myaccount.                                           //
+//*****************************************************************//
+
+if ($GLOBALS['pixie_user']) {
+?>
+<ul id="sub_nav_level_1">
+				  		<li><a href="?s=<?php print $s; ?>" title="<?php print $lang['nav2_home'];?>"<?php if (($s == "myaccount") && ($x != "myprofile")) { if ($x !== "logs") { echo " class=\"sub_nav_current_1\""; } }?>><?php print $lang['nav2_home'];?></a></li>
+						<li><a href="?s=<?php print $s; ?>&amp;x=myprofile" title="<?php print $lang['nav2_profile'];?>"<?php if ((!$do) && ($x == "myprofile")) { echo " class=\"sub_nav_current_1\""; }?>><?php print $lang['nav2_profile'];?></a></li>
+						<li><a href="?s=<?php print $s; ?>&amp;x=myprofile&amp;do=security" title="<?php print $lang['nav2_security'];?>"<?php if ($do == "security") { echo " class=\"sub_nav_current_1\""; }?>><?php print $lang['nav2_security'];?></a></li>
+						</ul>
+					</li>
+<?php
+}
+?>
