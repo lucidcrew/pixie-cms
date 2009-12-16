a2b75e4332766650e220093cf802bf9b7e65cce4
isa.worcs <isa.worcs@d6a5f73a-7f42-0410-bfaf-71582268690a>
2009-12-16 18:49:04 +0000

    Adding warnings for dir permissions when not writable in installer

--- branches/pixie102_bugfix/admin/install/index.php
+++ branches/pixie102_bugfix/admin/install/index.php
@@ -6,7 +6,7 @@
 // Title: Installer.                                               //
 //*****************************************************************//
 
-	error_reporting(0);	// turn off error reporting
+	//error_reporting(0);	// turn off error reporting
 	extract($_REQUEST); // access to form vars if register globals is off
 			
 	switch($step) {
@@ -652,6 +652,23 @@ visit: ".$site_url."admin to login.";
 		switch($step) {
 			case "4":
 				global $site_url;
+                                /** Checking Dir Permissions **/
+				$warn_flag = false;
+                                if ($handle = opendir('../../files')) {
+                                        while (false !== ($file = readdir($handle))) {
+						$path = "../../files/$file";
+                                                if ($file != "." && $file != "..") {
+							
+                                                        if (is_dir($path) && !(is_writable($path))) {
+							$warn_flag = true;
+                                                                echo "<font size='-1'>Directory files/$file is not writable.</font><br>\n";
+							}
+                                                }
+                                        }
+                                        closedir($handle);
+                                }
+				if ($warn_flag)
+					echo "<p><font color=red>Warning</font> The permissions of the directories above should be set to 777 (drwxrwxrwx) for uploads and caching to be enabled</p><hr>\n";
 		?>	
 		<p>Congratulations your site is now setup and ready to use, if you want more themes and modules
 		make sure you visit the <a href="http://www.getpixie.co.uk" title="Pixie">Pixie website</a>. Remember to delete the install directory within Pixie to secure your site.</p>
