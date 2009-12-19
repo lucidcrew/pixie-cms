356520c118782cbea7702f24c89fe8d73f0ffa14
isa.worcs <isa.worcs@d6a5f73a-7f42-0410-bfaf-71582268690a>
2009-12-19 18:38:42 +0000

    Improved page gen. time by not calling sterilise() unless really necessary

--- branches/pixie102_bugfix/admin/lib/lib_auth.php
+++ branches/pixie102_bugfix/admin/lib/lib_auth.php
@@ -45,9 +45,9 @@
 		
 		global $lang;
 		
-		$username = sterilise($username, true);
-		$password = sterilise($password, true);
-		$remember = sterilise($remember, true);
+		$username = sterilise_txt($username, true);
+		$password = sterilise_txt($password, true);
+		$remember = sterilise_txt($remember, true);
 	
 		$howmany = count(safe_rows("*","pixie_log","log_message = '".$lang['failed_login']."' and user_ip = '".$_SERVER["REMOTE_ADDR"]."' and log_time < utc_timestamp() and log_time > DATE_ADD(utc_timestamp(), INTERVAL -1 DAY)"));
 		
