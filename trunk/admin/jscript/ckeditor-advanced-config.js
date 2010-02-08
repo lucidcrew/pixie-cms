e9b793a512639e3653dfd26138d15ae67dc9f47a
tonywhite100 <tonywhite100@d6a5f73a-7f42-0410-bfaf-71582268690a>
2010-02-08 02:58:57 +0000

    BUGFIX. CKEditor snapshot added, seems quite stable and includes more languages, bugfixes, etc. I'd forgotten to string the variables along in the ckeditor configs using + signs, so the editor would only work in Opera. It will now work in all browsers. So it should be ready for testing. I also fixed the logout link appearing after you have logged out in the admin area's index.php file.

--- trunk/admin/jscript/ckeditor-advanced-config.js
+++ trunk/admin/jscript/ckeditor-advanced-config.js
@@ -4,30 +4,41 @@ Pixie advanced ckeditor config
 
 var cssShortPath = 'admin/themes/';
 var cssDir = cssShortPath + globalUrlVars.pixieThemeDir;
+var cssCssie
+var cssCssie6
+var cssCssie7
+var cssHandheld
 
 /* Use the optional stylesheets, if we've got them */
-if (globalUrlVars.cssCssie) { var cssCssie = ', ' + cssDir + globalUrlVars.cssCssie; }
-if (globalUrlVars.cssCssie6) { var cssCssie6 = ', ' + cssDir + globalUrlVars.cssCssie6; }
-if (globalUrlVars.cssCssie7) { var cssCssie7 = ', ' + cssDir + globalUrlVars.cssCssie7; }
-if (globalUrlVars.cssHandheld) { var cssHandheld = ', ' + cssDir + globalUrlVars.cssHandheld; }
+if (globalUrlVars.cssCssie) { cssCssie = ', ' + cssDir + globalUrlVars.cssCssie; }
+if (globalUrlVars.cssCssie6) { cssCssie6 = ', ' + cssDir + globalUrlVars.cssCssie6; }
+if (globalUrlVars.cssCssie7) { cssCssie7 = ', ' + cssDir + globalUrlVars.cssCssie7; }
+if (globalUrlVars.cssHandheld) { cssHandheld = ', ' + cssDir + globalUrlVars.cssHandheld; }
 
+var $j = jQuery.noConflict();
+
+if ($j('.ck-textarea').length >= 1) { 
 /* Tell ckeditor how we want it to be configured */
 CKEDITOR.editorConfig = function(config) {
 
 	config.baseHref = globalUrlVars.pixieSiteUrl;
 	config.docType = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
-	config.contentsCss = [ cssDir + '/core.css', cssDir + '/layout.css', cssDir + '/navigation.css' cssCssie cssCssie6 cssCssie7 cssHandheld ];
+	/* The next two aren't ready for go live yet, themes need work to make them work better in ckeditor */
+        /* config.bodyId = 'content'; */ /* Set this to see more of your theme inside the editor. Although it doesn't work entirely as expect in most cases. */
+	/* config.contentsCss = [ cssDir + '/core.css', cssDir + '/layout.css', cssDir + '/navigation.css' + cssCssie + cssCssie6 + cssCssie7 + cssHandheld ]; */
         config.filebrowserBrowseUrl = '?s=publish&x=filemanager&ck=1&ckfile=1';
         config.filebrowserImageBrowseUrl = '?s=publish&x=filemanager&ck=1&ckimage=1';
 	config.filebrowserWindowWidth = '800';
         config.filebrowserWindowHeight = '600';
 	/* Define changes to the advanced configuration here. For example: */
-	/* config.language = 'en-uk'; */ /* Not required. ckeditor automatically selects language based on what your browser is set to */
+	/* config.language = 'en-gb'; */ /* Not required. ckeditor automatically selects language based on what your browser is set to */
 	/* config.contentsLangDirection = 'rtl'; */  /* Unhash this setting if you are using a right to left language like Japanese */
 	config.skin = 'office2003'; /* Use config.skin = 'v2'; instead to make it look like it's tinymce */
 	config.emailProtection = 'encode'; /* Protect email links from spammers */
 	config.resize_enabled = true;
 	config.colorButton_enableMore = true;
+	/* config.removePlugins = 'elementspath'; */ /* Remove the name of elements at the bottom of the editor */
+	config.toolbarCanCollapse = false; /* Remove the collapsing button of the toolbar */
         config.toolbar = [
 	['Preview', '-', 'Cut', 'Copy', 'Paste', 'Find', 'Undo', 'Redo', '-', 'SelectAll', 'RemoveFormat', 'Table', 'HorizontalRule', 'PageBreak', 'ShowBlocks', '-', 'Templates', '-', 'Styles', 'Format', '-', 'Font', 'FontSize'],
 	'/',
@@ -37,3 +48,5 @@ CKEDITOR.editorConfig = function(config) {
 	config.shiftEnterMode = CKEDITOR.ENTER_P; /* Paragraphs are now made by pressing shift and enter together instead */
 
 };
+
+}/* End if class ck-textarea exists in the dom */
