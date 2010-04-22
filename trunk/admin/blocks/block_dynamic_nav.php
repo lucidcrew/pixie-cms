450fd6c9f0cf9bee2ae462f2e49f364651c17c50
sam.collett <sam.collett@d6a5f73a-7f42-0410-bfaf-71582268690a>
2010-04-22 12:14:18 +0000

    Applying Tony's fix for Issue 19

--- trunk/admin/blocks/block_dynamic_nav.php
+++ trunk/admin/blocks/block_dynamic_nav.php
@@ -37,89 +37,38 @@ if (!defined('DIRECT_ACCESS')) {
 global $s, $m, $x, $site_url, $lang;
 if ($nested_nav == 'yes') {
 ?>
-					<ul id="sub_navigation_1">
-						<li><a href="<?php
-	echo createURL($page_name, 'archives');
-?>" title="<?php
-	print $page_display_name . ': ' . $lang['archives'];
-?>"<?php
-	if ($m == 'archives') {
-		print " class=\"sub_nav_current_1 replace\"";
-	} else {
-		print " class=\"replace\"";
-	}
-?>><?php
-	print $lang['archives'];
-?><span></span></a></li>
-						<li><a href="<?php
-	echo createURL($page_name, 'popular');
-?>" title="<?php
-	print $page_display_name . ': ' . $lang['popular_posts'];
-?>"<?php
-	if ($m == 'popular') {
-		print " class=\"sub_nav_current_1 replace\"";
-	} else {
-		print " class=\"replace\"";
-	}
-?>><?php
-	print $lang['popular_posts'];
-?><span></span></a></li>
-						<li><a href="<?php
-	echo createURL($page_name, 'tags');
-?>" title="<?php
-	print $page_display_name . ': ' . $lang['tags'];
-?>"<?php
-	if ($m == 'tags') {
-		print " class=\"sub_nav_current_1 replace\"";
-	} else {
-		print " class=\"replace\"";
-	}
-?>><?php
-	print $lang['tags'];
-?><span></span></a></li>
-					</ul>
-					</li>
+	<ul id="sub_navigation_1">
+		<li>
+			<a href="<?php echo createURL($page_name, 'archives'); ?>" title="<?php echo "{$page_display_name}: {$lang['archives']}"; ?>"<?php if ($m == 'archives') { echo " class=\"sub_nav_current_1 replace\""; } else { echo " class=\"replace\""; } ?>><?php echo $lang['archives']; ?>
+				<span></span>
+			</a>
+		</li>
+		<li>
+			<a href="<?php echo createURL($page_name, 'popular'); ?>" title="<?php echo "{$page_display_name}: {$lang['popular_posts']}"; ?>"<?php if ($m == 'popular') { echo " class=\"sub_nav_current_1 replace\""; } else { echo " class=\"replace\""; } ?>><?php echo $lang['popular_posts']; ?>
+				<span></span>
+			</a>
+		</li>
+		<li>
+			<a href="<?php echo createURL($page_name, 'tags'); ?>" title="<?php echo "{$page_display_name}: {$lang['tags']}"; ?>"<?php if ($m == 'tags') { echo " class=\"sub_nav_current_1 replace\""; } else { echo " class=\"replace\""; } ?>><?php echo $lang['tags']; ?>
+				<span></span>
+			</a>
+		</li>
+	</ul>
 <?php
-} else {
+	} else {
 	echo "\t\t\t\t\t<div id=\"block_dynamic_nav\" class=\"block\">\n\t\t\t\t\t\t<div class=\"block_header\">\n\t\t\t\t\t\t\t<h4>Sub Navigation</h4>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class=\"block_body\">\n";
 ?>
-							<ul id="sub_navigation_1">
-								<li><a href="<?php
-	echo createURL($page_name, 'archives');
-	;
-?>" title="<?php
-	print $page_display_name . ': ' . $lang['archives'];
-?>"<?php
-	if ($m == 'archives') {
-		print " class=\"sub_nav_current_1\"";
-	}
-?>><?php
-	print $lang['archives'];
-?></a></li>
-								<li><a href="<?php
-	echo createURL($page_name, 'popular');
-?>" title="<?php
-	print $page_display_name . ': ' . $lang['popular_posts'];
-?>"<?php
-	if ($m == 'popular') {
-		print " class=\"sub_nav_current_1\"";
-	}
-?>><?php
-	print $lang['popular_posts'];
-?></a></li>
-								<li><a href="<?php
-	echo createURL($page_name, 'tags');
-?>" title="<?php
-	print $page_display_name . ': ' . $lang['tags'];
-?>"<?php
-	if ($m == 'tags') {
-		print " class=\"sub_nav_current_1\"";
-	}
-?>><?php
-	print $lang['tags'];
-?></a></li>
-							</ul>
+	<ul id="sub_navigation_1">
+		<li>
+			<a href="<?php echo createURL($page_name, 'archives'); ?>" title="<?php echo "{$page_display_name}: {$lang['archives']}"; ?>"<?php if ($m == 'archives') { echo " class=\"sub_nav_current_1\""; } ?>><?php echo $lang['archives']; ?></a>
+		</li>
+		<li>
+			<a href="<?php echo createURL($page_name, 'popular'); ?>" title="<?php echo "{$page_display_name}: {$lang['popular_posts']}"; ?>"<?php if ($m == 'popular') { echo " class=\"sub_nav_current_1\""; } ?>><?php echo $lang['popular_posts']; ?></a>
+		</li>
+		<li>
+			<a href="<?php echo createURL($page_name, 'tags'); ?>" title="<?php echo "{$page_display_name}: {$lang['tags']}"; ?>"<?php if ($m == 'tags') { echo " class=\"sub_nav_current_1\""; } ?>><?php echo $lang['tags']; ?></a>
+		</li>
+	</ul>
 <?php
 	echo "\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class=\"block_footer\"></div>\n\t\t\t\t\t</div>\n";
 }
-?>
